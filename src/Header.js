874a13ab524f03aa0236f90563d1dac9ea048b48
Abhay <26abhaykumarsingh@gmail.com>
2023-06-13 23:19:25 +0530

    first commit

---
+++ src/Header.js
@@ -0,0 +1,19 @@
+import React from "react";
+import "./Header.css";
+import PersonIcon from "@mui/icons-material/Person";
+import TinderLogo from "./tinderLogo.png";
+import IconButton from "@mui/material/IconButton";
+
+function Header() {
+  return (
+    <div className="header">
+      <IconButton>
+        <PersonIcon fontSize="large" className="header__icon" />
+      </IconButton>
+
+      <img className="header__logo" src={TinderLogo} alt="" />
+    </div>
+  );
+}
+
+export default Header;
